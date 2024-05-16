b204659bd901a72f1816b908ddd6d371d362fb26
joma-Dev <shakibul.islam@jomma.online>
2024-05-16 11:12:50 +0600

    "Added QuestionCard component, updated Details page with new layout and content, and added custom font for Bengali language"

--- src/pages/[...details].js
+++ src/pages/[...details].js
@@ -5,6 +5,39 @@ import RootLayout from '@/components/Layouts/RootLayout';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import QuestionCard from '@/components/Shared/QuestionCard';
+const data2 = [
+	{
+		id: 1,
+		title_en: 'What does Jomma do?',
+		title_bn: 'জমার কাজ কি ?',
+	},
+	{
+		id: 2,
+		title_en: 'What is required to open a broker account online?',
+		title_bn: 'অনলাইনে ব্রোকার একাউন্ট খুলতে কি কি লাগে ? ',
+	},
+	{
+		id: 3,
+		title_en: 'What is the top 10 shares to invest?',
+		title_bn: 'বিনিয়োগের জন্য সেরা ১০ টি শেয়ারের নাম কি কি ?',
+	},
+	{
+		id: 4,
+		title_en: 'What is the benefit of investing in mutual fund?',
+		title_bn: 'মিচ্যুয়াল ফান্ডে বিনিয়োগের সুবিধা কি কি ?',
+	},
+	{
+		id: 5,
+		title_en: 'What is Brac EPL?',
+		title_bn: 'ব্র্যাক ইপিএল ওয়েলথ ম্যানেজমেন্ট কি ?',
+	},
+	{
+		id: 6,
+		title_en: 'How can I play trading game?',
+		title_bn: 'বিনিয়োগের সিমুলেশন গেইম কীভাবে খেলবো ?',
+	},
+];
 
 const Details = () => {
 	return (
@@ -15,6 +48,7 @@ const Details = () => {
 					name="viewport"
 					content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
 				/>
+				
 			</Head>
 
 			<div className="container mt-4">
@@ -32,10 +66,31 @@ const Details = () => {
 						style={{ marginLeft: '-6px' }}
 						className="show-logo-mobile"
 					/>
-						<h1 className="video-section-header mt-2">
-							About Jomma | জমা সম্পর্কে তথ্য
+						<h1 className="video-section-header mt-2 mb-3 mb-md-4">
+							About Jomma | <span style={{ fontFamily: 'Noto Sans Bengali' }}>জমা সম্পর্কে তথ্য</span>
 						</h1>
-				</div>
+
+						<div className='w-100' style={{height:'1px', backgroundColor:'#A5A5A7'}}></div>
+
+						<div className='details-area'>
+						<p>
+						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy.
+						</p>
+
+						<p style={{ fontFamily: 'Noto Sans Bengali' }}>
+						এটি একটি দীর্ঘ প্রতিষ্ঠিত সত্য যে একটি পাঠক একটি পৃষ্ঠার পাঠযোগ্য বিষয়বস্তু দ্বারা বিভ্রান্ত হবে যখন এটির বিন্যাসটি দেখবে। Lorem Ipsum ব্যবহার করার বিষয় হল যে এটিতে কন্টেন্ট এখানে, এখানে বিষয়বস্তু ব্যবহার করার বিপরীতে অক্ষরের কম-বেশি স্বাভাবিক বন্টন রয়েছে, এটিকে পঠনযোগ্য ইংরেজির মতো দেখায়। অনেক ডেস্কটপ পাবলিশিং প্যাকেজ এবং ওয়েব পেজ এডিটররা এখন তাদের ডিফল্ট মডেল টেক্সট হিসেবে Lorem Ipsum ব্যবহার করে এবং -এর জন্য একটি অনুসন্ধান অনেক ওয়েব সাইটকে তাদের শৈশবকালে উন্মোচিত করবে
+						</p>
+
+						</div>
+
+
+						<div className='mt-3 mt-md-5'>
+						<QuestionCard topics={data2} title={'Frequently Asked Questions'}/>
+						</div>
+
+
+						
+			</div>
 			</div>
 		</>
 	);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { store } from "@/redux/store";
 import "@/styles/globals.css";
 import Script from "next/script";
@@ -29,6 +30,12 @@ export default function App({ Component, pageProps }) {
       <meta name="format-detection" content="telephone=no" />
       {/* Add Bootstrap CSS */}
       <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wdth,wght@100,100..900&display=swap"
+          rel="stylesheet"
+        />
       
         <main className={inter.className}>
           <Provider store={store}>
